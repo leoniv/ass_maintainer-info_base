b3f04da6f235ab46900ba25199204eb1b7d27137
Leonid Vlasov <leoniv.vlasov@gmail.com>
2016-12-02 12:16:41 +0700

    extracts info_base/cfg.rb

--- lib/ass_maintainer/info_base.rb
+++ lib/ass_maintainer/info_base.rb
@@ -25,89 +25,12 @@ module AssMaintainer
     require 'ass_maintainer/info_base/interfaces'
     require 'ass_maintainer/info_base/file_ib'
     require 'ass_maintainer/info_base/server_ib'
+    require 'ass_maintainer/info_base/cfg'
 
     # :nodoc:
     class MethodDenied < StandardError
       def initialize(m)
         super "Infobase is read only. Method #{m} denied!"
-      end
-    end
-
-    # @abstract
-    class AbstractCfg
-      attr_reader :infobase
-      # @param infobase [InfoBase]
-      def initialize(infobase)
-        @infobase = infobase
-      end
-    end
-
-    # Object for manipuate whith infobase configuration
-    class Cfg < AbstractCfg
-      # Dump configuration to +XML+ files
-      # @param path [String]
-      # @return [String] path
-      def dump_xml(path)
-        infobase.designer do
-          dumpConfigToFiles path
-        end.run.wait.result.verify!
-        path
-      end
-
-      # Dump configuration to +.cf+ file
-      # @param path [String]
-      # @return [String] path
-      def dump(path)
-        infobase.designer do
-          dumpCfg path
-        end.run.wait.result.verify!
-        path
-      end
-
-      # Load configuration from +XML+ files
-      # @param path [String]
-      # @return [String] path
-      def load_xml(path)
-        fail MethodDenied, :load_xml if infobase.read_only?
-        infobase.designer do
-          loadConfigFromFiles path
-        end.run.wait.result.verify!
-        path
-      end
-
-      # Load configuration from +.cf+ file
-      # @param path [String]
-      # @return [String] path
-      def load(path)
-        fail MethodDenied, :load_cf if infobase.read_only?
-        infobase.designer do
-          loadCfg path
-        end.run.wait.result.verify!
-        path
-      end
-    end
-
-    # Object for manipuate whith database configuration
-    class DbCfg < AbstractCfg
-      # Update database configuration from infobase
-      # configuration
-      def update
-        fail MethodDenied, :update if infobase.read_only?
-        infobase.designer do
-          updateDBCfg do
-            warningsAsErrors
-          end
-        end.run.wait.result.verify!
-      end
-
-      # Dump configuration to +.cf+ file
-      # @param path [String]
-      # @return [String] path
-      def dump(path)
-        infobase.designer do
-          dumpDBCfg path
-        end.run.wait.result.verify!
-        path
       end
     end
 
